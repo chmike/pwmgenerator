cacf287d555541656538e2298e8a2817d0e36743
Christophe Meessen <christophe@meessen.net>
2022-03-16 19:15:00 +0100

    set required kernel parameters

--- main.c
+++ main.c
@@ -25,6 +25,23 @@ int main(int argc, char *argv[]) {
   } else
     port = 1234;
 
+
+  FILE *f = fopen("/proc/sys/kernel/sched_rt_runtime_us", "w");
+  if(f == NULL) {
+    fprintf(stderr,"failed writing -1 to /proc/sys/kernel/sched_rt_runtime_us");
+    return 1;
+  }
+  fprintf(f, "-1");
+  fclose(f);
+
+  f = fopen("/sys/devices/system/cpu/cpu3/cpufreq/scaling_governor", "w");
+  if(f == NULL) {
+    fprintf(stderr,"failed writing performance to /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor");
+    return 1;
+  }
+  fprintf(f, "performance");
+  fclose(f);
+
   // initialize GPIO
   int res = gpio_init();
   if(res < 0)
