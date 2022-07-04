de82c8ed63a16ab2f24a1ef55a761c8729a67ecb
Christophe Meessen <christophe@meessen.net>
2022-07-04 15:31:39 +0200

    set time tick pause to 2000

--- generator.c
+++ generator.c
@@ -23,7 +23,7 @@ volatile double frequencyVariance;               // frequency variance with expo
 char stats[16];
 volatile uint64_t dummy; 
 
-#define PAUSE_VALUE 9500
+#define PAUSE_VALUE 2000
 
 uint64_t getTimeStamp() {
   struct timespec t;
